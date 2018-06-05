6c78d2c0cb7ab6296798d462b2faed68c34a227b
Emiliano Cervantes <ovman96@hotmail.com>
2018-06-05 17:23:07 -0500

    Nueva forma

--- hello2.py
+++ hello2.py
@@ -5,6 +5,16 @@ sense = SenseHat()
 r=(255,0,0)
 e=(0,0,0)
 space=[
+	e,e,e,e,e,e,e,e,
+	e,r,r,e,r,r,e,e,
+	r,r,r,r,r,r,r,e,
+	r,r,r,r,r,r,r,e,
+	e,r,r,r,r,r,e,e,
+	e,e,r,r,r,e,e,e,
+	e,e,e,r,e,e,e,e,
+	e,e,e,e,e,e,e,e,
+]
+'''space=[
 	e,e,e,r,r,e,e,e,
 	e,e,r,r,r,r,e,e,
 	e,r,r,r,r,r,r,e,
@@ -13,6 +23,6 @@ space=[
 	e,e,r,e,e,r,e,e,
 	e,r,e,r,r,e,r,e,
 	r,e,r,e,e,r,e,r,
-]
+]'''
 sense.set_pixels(space)
 sense.set_rotation(180)
